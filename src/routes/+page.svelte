9d8fa5e64dc8d0d950e4001d4cecd2bf3d189caa
akTARDIGRADE13 <itmtha@gmail.com>
2023-06-05 09:34:17 +0900

    Fix title-page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,27 @@
-<h1 class=title>ICPC-Library</h1>
-<hr>
-<div>
-  <p>ICPC用のライブラリを整備しています。</p>
-  <p>今のところC++のみです。</p>
-</div>
+<body>
+  <h1 class=title>ICPC-Library</h1>
+  <hr>
+  <div class=text>
+    <p>ICPC用のライブラリを整備しています。</p>
+    <p>今のところC++のみです。</p>
+  </div>
+</body>
 
 <style>
-  * {
+  body {
+    width: 100%;
+    height: 100%;
     background-color: rgb(234, 234, 234);
   }
 
   .title {
-    font-size: 30px;
+    font-size: 40px;
     color:rgb(0, 69, 112);
+  }
+
+  .text {
+    width: 100%;
+    height: 100%;
+    font-size: 20px;
   }
 </style>
